1118c44e0d7094a42293ea390958867e2fdb9499
NickHimmel <himmel.nick@gmail.com>
2020-09-12 17:23:04 -0400

    Add axios and make api call to Hacker News for react hooks articles. Console log response for now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios'
 
 export default function App() {
+  useEffect(() => {
+    axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+      .then(response => {
+        console.log(response.data)
+      })
+  })
+
   return (
     <div className="App">
       App
